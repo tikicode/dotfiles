0096b8dcbd9b630efbb66459766a266e200118a5
tikicode <anukala1@jhu.edu>
2023-02-04 17:24:28 -0500

    initial

---
+++ nvim/lua/avnukala/mappings.lua
@@ -0,0 +1,13 @@
+local map = vim.keymap.set -- mappings
+
+-- Change escape to jk
+map("i", "jk", "<ESC>", options)
+
+-- Indenting
+map("n", "<M-]>", ">>", s_opts)
+map("n", "<M-[>", "<<", s_opts)
+
+-- Save key strokes so we don't need to press shift to enter command mode
+map({"n", "x"}, ";", ":")
+
+
